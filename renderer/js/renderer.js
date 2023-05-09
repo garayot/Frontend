405362ea2be445c73c5ede6efa4cd063ccf0f267
garayot <garayot@gmail.com>
2023-05-10 02:13:53 +0800

    added axios function to access openAI API

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,12 +1,19 @@
 const form = document.getElementById("ingredients");
-if(form){
-    form.onsubmit = function(e){
-        e.preventDefault();
 
-        const formData = new FormData(form);
+if (form) {
+  form.onsubmit = async function (e) {
+    e.preventDefault();
 
-        for (const [key, value] of formData) {
-            console.log(`${key}: ${value}\n`);
-          }
-    };
+    const formData = new FormData(form);
+    const response = await window.axios.openAI(formData.get("ingredients"));
+
+    const ingredientsList = response.choices[0].text
+      .split('- ') // Split the response into separate ingredients
+      .map((ingredient) => ingredient.trim()) // Remove leading/trailing whitespace from each ingredient
+      .filter((ingredient) => ingredient !== ''); // Remove any empty lines
+
+    const ingredientsText = ingredientsList.join('\n'); // Join the ingredients together with line breaks
+
+    document.getElementById("recipe").textContent = ingredientsText;
+  };
 }
